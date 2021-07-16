e48975e7cde0101210f45ae5f15dd0f896e9ba97
the127 <julian.baehr@googlemail.com>
2021-07-17 00:05:38 +0200

    format and add tests

--- Toolkit/NewAppDeployToolkit/Functions/Main/Get-HardwarePlatform.ps1
+++ Toolkit/NewAppDeployToolkit/Functions/Main/Get-HardwarePlatform.ps1
@@ -1,4 +1,3 @@
-#region Function Get-HardwarePlatform
 Function Get-HardwarePlatform {
 <#
 .SYNOPSIS
@@ -15,42 +14,50 @@ Function Get-HardwarePlatform {
 #>
 	[CmdletBinding()]
 	Param (
-		[Parameter(Mandatory=$false)]
-		[ValidateNotNullorEmpty()]
-		[boolean]$ContinueOnError = $true
+		[boolean]$ContinueOnError = $True
 	)
 
 	Begin {
-		## Get the name of this function and write header
-		[string]${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name
-		Write-FunctionInfo -CmdletName ${CmdletName} -CmdletBoundParameters $PSBoundParameters -Header
+		$CmdletName = $PSCmdlet.MyInvocation.MyCommand.Name
+		Write-FunctionInfo -CmdletName $CmdletName -CmdletBoundParameters $PSBoundParameters -Header
 	}
+
 	Process {
 		Try {
-			Write-Log -Message 'Retrieving hardware platform information.' -Source ${CmdletName}
+			Write-Log -Message 'Retrieving hardware platform information.' -Source $CmdletName
+			
 			$hwBios = Get-WmiObject -Class 'Win32_BIOS' -ErrorAction 'Stop' | Select-Object -Property 'Version', 'SerialNumber'
 			$hwMakeModel = Get-WMIObject -Class 'Win32_ComputerSystem' -ErrorAction 'Stop' | Select-Object -Property 'Model', 'Manufacturer'
 
-			If ($hwBIOS.Version -match 'VRTUAL') { $hwType = 'Virtual:Hyper-V' }
-			ElseIf ($hwBIOS.Version -match 'A M I') { $hwType = 'Virtual:Virtual PC' }
-			ElseIf ($hwBIOS.Version -like '*Xen*') { $hwType = 'Virtual:Xen' }
-			ElseIf ($hwBIOS.SerialNumber -like '*VMware*') { $hwType = 'Virtual:VMWare' }
-			ElseIf (($hwMakeModel.Manufacturer -like '*Microsoft*') -and ($hwMakeModel.Model -notlike '*Surface*')) { $hwType = 'Virtual:Hyper-V' }
-			ElseIf ($hwMakeModel.Manufacturer -like '*VMWare*') { $hwType = 'Virtual:VMWare' }
-			ElseIf ($hwMakeModel.Model -like '*Virtual*') { $hwType = 'Virtual' }
-			Else { $hwType = 'Physical' }
+			If ($hwBIOS.Version -match 'VRTUAL') {
+				 $hwType = 'Virtual:Hyper-V' 
+			} ElseIf ($hwBIOS.Version -match 'A M I') {
+				 $hwType = 'Virtual:Virtual PC' 
+			} ElseIf ($hwBIOS.Version -like '*Xen*') { 
+				$hwType = 'Virtual:Xen' 
+			} ElseIf ($hwBIOS.SerialNumber -like '*VMware*') { 
+				$hwType = 'Virtual:VMWare' 
+			} ElseIf (($hwMakeModel.Manufacturer -like '*Microsoft*') -and ($hwMakeModel.Model -notlike '*Surface*')) { 
+				$hwType = 'Virtual:Hyper-V' 
+			} ElseIf ($hwMakeModel.Manufacturer -like '*VMWare*') { 
+				$hwType = 'Virtual:VMWare' 
+			} ElseIf ($hwMakeModel.Model -like '*Virtual*') { 
+				$hwType = 'Virtual' 
+			} Else { 
+				$hwType = 'Physical' 
+			}
 
 			Write-Output -InputObject $hwType
-		}
-		Catch {
-			Write-Log -Message "Failed to retrieve hardware platform information. `r`n$(Resolve-Error)" -Severity 3 -Source ${CmdletName}
+		} Catch {
+			Write-Log -Message "Failed to retrieve hardware platform information. `r`n$(Resolve-Error)" -Severity 3 -Source $CmdletName
+			
 			If (-not $ContinueOnError) {
 				Throw "Failed to retrieve hardware platform information: $($_.Exception.Message)"
 			}
 		}
 	}
+
 	End {
-		Write-FunctionInfo -CmdletName ${CmdletName} -Footer
+		Write-FunctionInfo -CmdletName $CmdletName -Footer
 	}
 }
-#endregion
